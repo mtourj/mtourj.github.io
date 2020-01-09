ab56fb2082488619621a908185072a2555ae3ebd
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2020-01-09 12:23:12 -0800

    Add project Scribe to portfolio

--- src/App.js
+++ src/App.js
@@ -1,55 +1,67 @@
-import React from 'react';
+import React from "react";
 
-import Project from './components/Project';
-import Info from './components/Info';
+import Project from "./components/Project";
+import Info from "./components/Info";
 
-import conjugator from './img/conjugator.png';
-import devmap from './img/devmap.png';
-import designhub from './img/designhub.png';
+import conjugator from "./img/conjugator.png";
+import scribe from "./img/scribe.png";
+import designhub from "./img/designhub.png";
 
-
-import './App.scss';
+import "./App.scss";
 
 function App() {
   const projects = [
     {
-      name: 'DesignHub X',
-      code: 'https://github.com/Lambda-School-Labs/designhub-fe',
-      demo: 'https://www.designhubx.com/',
-      image: designhub,
-      description: 'DesignHub is a collaborative and intuitive platform for artists and designers to share and manage their work. ',
-      tech: 'React/Redux, NodeJS/Express, PostgreSQL'
+      name: "Scribe",
+      code: "https://github.com/Breath-Taken/",
+      demo: "https://notaking-with-scribe.netlify.com/",
+      image: scribe,
+      early: true,
+      description:
+        "Scribe is a digital note-taker. It allows a user to record transcripts by listening to their speech/meeting/conversation and storing it into their personal repository of transcripts"
     },
     {
-      name: 'Conjugator',
-      code: 'https://github.com/ls-bw-06212019-conjugator/conjugator-FE',
-      demo: 'https://bw-conjugator.netlify.com/',
-      image: conjugator,
-      description: 'Conjugator is an educational, drill-practice style app that helps users practice Spanish conjugations through repitition and visual/statistical feedback.',
-      tech: 'React/Redux, NodeJS/Express, PostgreSQL',
-      early: false
+      name: "DesignHub X",
+      code: "https://github.com/Lambda-School-Labs/designhub-fe",
+      demo: "https://www.designhubx.com/",
+      image: designhub,
+      description:
+        "DesignHub is a collaborative and intuitive platform for artists and designers to share and manage their work. ",
+      tech: "React/Redux, NodeJS/Express, PostgreSQL"
     },
-  ]
+    {
+      name: "Conjugator",
+      code: "https://github.com/ls-bw-06212019-conjugator/conjugator-FE",
+      demo: "https://bw-conjugator.netlify.com/",
+      image: conjugator,
+      description:
+        "Conjugator is an educational, drill-practice style app that helps users practice Spanish conjugations through repitition and visual/statistical feedback.",
+      tech: "React/Redux, NodeJS/Express, PostgreSQL",
+      early: false
+    }
+  ];
 
   return (
     <div className="App">
-      <div className='container-left'>
-        <hr className='horizontal-line' />
-        <h1 className='head'>MOHAMMAD TOURJOMAN</h1>
-        <h1 className='subhead'>Software Developer</h1>
-        <h4 className='subhead'>Eastvale, California</h4>
-        <h4 className='subhead'>React/Redux, NodeJS/Express, SQLite/PostgreSQL/MongoDB</h4>
+      <div className="container-left">
+        <hr className="horizontal-line" />
+        <h1 className="head">MOHAMMAD TOURJOMAN</h1>
+        <h1 className="subhead">Software Developer</h1>
+        <h4 className="subhead">Eastvale, California</h4>
+        <h4 className="subhead">
+          React/Redux, NodeJS/Express, SQLite/PostgreSQL/MongoDB
+        </h4>
         <Info />
-        <hr className='horizontal-line' />
+        <hr className="horizontal-line" />
       </div>
-      <div className='container-right'>
-      <h1 className='head'>MOST RECENT PROJECTS</h1>
-      {projects.map(project => <Project key={project.name} data={project}/>)}
-      More to come...
+      <div className="container-right">
+        <h1 className="head">MOST RECENT PROJECTS</h1>
+        {projects.map(project => (
+          <Project key={project.name} data={project} />
+        ))}
+        More to come...
       </div>
-      <div className='arrow-down'>
-      &#8595;
-      </div>
+      <div className="arrow-down">&#8595;</div>
     </div>
   );
 }
